c137980fb4afc57292c8e99c5e836fc9fa709d66
Iro Enezli <enezliro@gmail.com>
2018-11-30 13:13:05 +0200

    ajax request with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
+
 const API_KEY = '39a393c6d2e48576fa1e516631fd7000';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export const fetchWeather = (city) => {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
