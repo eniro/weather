c25318d2b115aa561f2ff523edc19b0df2a9a3f5
Iro Enezli <enezliro@gmail.com>
2018-11-30 16:48:01 +0200

    mapping pros

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,10 +1,10 @@
-import { FETCH_WEATHER } from '../actions/index'
+import { FETCH_WEATHER } from '../actions/index';
 
 
 export default (state = null, action) => {
     switch (action.type) {
         case FETCH_WEATHER:
-            return [ action.payload.data, ...state ];
+            return [action.payload.data, ...state];
     }
     return state;
 }
