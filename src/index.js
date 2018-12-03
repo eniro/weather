a148dc64752be052318c96d3c13b716da44ded79
Iro Enezli <enezliro@gmail.com>
2018-12-03 12:30:45 +0200

    updated index

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
+
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
